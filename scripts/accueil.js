d8757ea2cdd9c97f8b20f1814a2a2c5ccfeb5ce8
Émilie Fontaine <emilie@fontaine.net>
2021-03-12 01:23:33 -0500

    Update accueil.js

--- scripts/accueil.js
+++ scripts/accueil.js
@@ -1,1 +1,25 @@
-
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    effect: "coverflow",
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    
+    
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  
