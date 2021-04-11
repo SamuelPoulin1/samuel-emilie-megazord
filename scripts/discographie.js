980781e8c80a94080a01e22490bc70149a462137
SamuelPoulin1 <70410165+SamuelPoulin1@users.noreply.github.com>
2021-04-11 13:17:38 -0400

    Complet

--- scripts/discographie.js
+++ scripts/discographie.js
@@ -1,84 +1,111 @@
 /*Swiper Carrousel*/
 const swiper = new Swiper('.swiper-container', {
-    effect: 'flip',
-    direction: 'vertical',
-    loop: true,
-    autoplay: {
-     delay: 2000,
-   },
-  });
-  
-  /*Animation de section*/
-  gsap.registerPlugin(ScrollTrigger);
-  
-  const section01 = document.querySelector('.section01');
-  const sectionTitre01 = document.querySelector('.sectionTitre');
-  const sectionTitre02 = document.querySelector('.sectionTitre');
-  const sectionTitre03 = document.querySelector('.sectionTitre');
-  const album1 = document.querySelector('.album1');
-  const album2 = document.querySelector('.album2');
-  const album3 = document.querySelector('.album3');
-  const section02 = document.querySelector('.section02');
-  const sectionTitre04 = section02.querySelector('.sectionTitre');
-  const main = document.querySelector('main');
-  
-  gsap.timeline({
-     scrollTrigger: {
-     trigger: ".section01",
-       start: 'top top',
-     end: 'bottom bottom',
-       ease: 'back', 
-       toggleActions: 'restart none reverse none',
+  effect: 'flip',
+  direction: 'vertical',
+  loop: true,
+  autoplay: {
+   delay: 2000,
+ },
+});
+
+/*Animation de section*/
+gsap.registerPlugin(ScrollTrigger);
+
+const section01 = document.querySelector('.section01');
+const sectionTitre01 = document.querySelector('.sectionTitre');
+const sectionTitre02 = document.querySelector('.sectionTitre');
+const sectionTitre03 = document.querySelector('.sectionTitre');
+const section02 = document.querySelector('.section02');
+const sectionTitre04 = section02.querySelector('.sectionTitre');
+
+gsap.timeline({
+   scrollTrigger: {
+   trigger: ".section01",
+	 start: 'top top',
+   end: 'bottom bottom',
+	 ease: 'back', 
+	 toggleActions: 'restart none reverse none',
+    }
+  })
+
+.from(".section01", {
+  scrollTrigger: ".section01",
+  ease: 'back',
+  opacity: 0,
+  y: -200,
+  duration: 1,
+})
+
+.from(".sectionTitre01", {
+  scrollTrigger: ".section01",
+  ease: 'back',
+  opacity: 0,
+  x: -600,
+  duration: 1,
+  scale: 3,
+})
+
+.from(".sectionTitre02", {
+  scrollTrigger: ".section01",
+  ease: 'back',
+  opacity: 0,
+  x: -600,
+  duration: 1,
+  scale: 3,
+})
+
+.from(".sectionTitre03", {
+  scrollTrigger: ".section01",
+  ease: 'back',
+  opacity: 0,
+  x: -600,
+  duration: 1,
+  scale: 3,
+})
+
+gsap.from(".section02", {
+  scrollTrigger: ".section02",
+  ease: 'back',
+  opacity: 0,
+  y: -100,
+  duration: 2,
+})
+
+gsap.from(".sectionTitre04", {
+  scrollTrigger: ".section02",
+  ease: 'back',
+  opacity: 0,
+  scale: 1.5,
+  duration: 1.5,
+})
+
+/*Animation Scroll*/
+
+gsap.registerPlugin(ScrollTrigger);
+
+let timeout;
+let body = document.body;
+const animationLogo = document.querySelector('.animationLogo')
+
+gsap.to('.animationLogo', {
+  scrollTrigger: {
+   scrub: true,
+    trigger: '.animationLogo',
+    onUpdate: (e) => {
+      body.classList.add('is-scrolling');
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        body.classList.remove('is-scrolling');
+      }, 300)
+      
+      if(e.direction == 1) {
+        body.classList.add('direction-down');
+        body.classList.remove('direction-up');
+      } 
+      if(e.direction == -1) {
+        body.classList.remove('direction-down');
+        body.classList.add('direction-up');
       }
-    })
-  
-  .from(".section01", {
-    scrollTrigger: ".section01",
-    ease: 'back',
-    opacity: 0,
-    y: -200,
-    duration: 1,
-  })
-  
-  .from(".sectionTitre01", {
-    scrollTrigger: ".section01",
-    ease: 'back',
-    opacity: 0,
-    x: -600,
-    duration: 1,
-    scale: 3,
-  })
-  
-  .from(".sectionTitre02", {
-    scrollTrigger: ".section01",
-    ease: 'back',
-    opacity: 0,
-    x: -600,
-    duration: 1,
-    scale: 3,
-  })
-  
-  .from(".sectionTitre03", {
-    scrollTrigger: ".section01",
-    ease: 'back',
-    opacity: 0,
-    x: -600,
-    duration: 1,
-    scale: 3,
-  })
-  
-  gsap.from(".section02", {
-    scrollTrigger: ".section02",
-    ease: 'back',
-    opacity: 0,
-    y: -100,
-    duration: 2,
-  })
-  
-  gsap.from(".sectionTitre04", {
-    scrollTrigger: ".section02",
-    ease: 'back',
-    opacity: 0,
-    scale: 1.5,
-    duration: 1.5,
-  })
+    }
+  }
+})
